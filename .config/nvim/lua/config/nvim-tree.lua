b6a561801f72f6689ab3b660407a3533ee97be85
Gang Zhuo <gang.zhuo@gmail.com>
2022-11-17 21:23:56 +0800

    config nvim-tree

--- .config/nvim/lua/config/nvim-tree.lua
+++ .config/nvim/lua/config/nvim-tree.lua
@@ -1,162 +1,79 @@
 local keymap = vim.keymap
 local nvim_tree = require("nvim-tree")
 
+local inject_node = require("nvim-tree.utils").inject_node
+
+local function edit(mode, node)
+  local path = node.absolute_path
+  if node.link_to and not node.nodes then
+    path = node.link_to
+  end
+  require("nvim-tree.actions.node.open-file").fn(mode, path)
+end
+
+local expand = function(node)
+  if node.nodes then
+    if not node.open then
+      require("nvim-tree.lib").expand_or_collapse(node)
+    end
+  elseif node.parent then
+    edit("edit", node)
+  end
+end
+
+local collapse = function(node)
+  if node.nodes and node.open then
+    require("nvim-tree.lib").expand_or_collapse(node)
+    return
+  end
+
+  -- find parent node which is opened
+  local p = node
+  while p.parent do
+      p = p.parent
+      if p.open then
+          break
+      end
+  end
+
+  -- if found and not a root node, collapse the node
+  if p and p ~= node and p.parent and p.open then
+    require("nvim-tree.lib").expand_or_collapse(p)
+    require("nvim-tree.utils").focus_file(p.absolute_path)
+  end
+end
+
+local set_keymap = function(key, fun, desc)
+  vim.keymap.set("n", key, inject_node(fun), {
+    silent = true,
+    buffer = bufnr,
+    desc = desc
+  })
+end
+
+local attach = function(bufnr)
+    set_keymap("l", expand, "expand folder/edit file")
+    set_keymap("h", collapse, "collapse folder")
+end
+
 nvim_tree.setup {
-  auto_reload_on_write = true,
-  disable_netrw = false,
-  hijack_cursor = false,
-  hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
-  open_on_tab = false,
-  sort_by = "name",
-  update_cwd = false,
-  remove_keymaps = false,
-  on_attach = function(bufnr)
-    local inject_node = require("nvim-tree.utils").inject_node
-    local function edit(mode, node)
-      local path = node.absolute_path
-      if node.link_to and not node.nodes then
-        path = node.link_to
-      end
-      require("nvim-tree.actions.node.open-file").fn(mode, path)
-    end
-    local expand = function(node)
-      if node.nodes then
-        if not node.open then
-          require("nvim-tree.lib").expand_or_collapse(node)
-        end
-      elseif node.parent then
-        edit("edit", node)
-      end
-    end
-    local collapse = function(node)
-      if node.nodes and node.open then
-        require("nvim-tree.lib").expand_or_collapse(node)
-        return
-      end
-
-      -- find parent node which is opened
-      local p = node
-      while p.parent do
-          p = p.parent
-          if p.open then
-              break
-          end
-      end
-
-      -- if found and not a root node, collapse the node
-      if p and p ~= node and p.parent and p.open then
-        require("nvim-tree.lib").expand_or_collapse(p)
-        require("nvim-tree.utils").focus_file(p.absolute_path)
-      end
-    end
-    local set_keymap = function(key, fun)
-      vim.keymap.set("n", key, inject_node(fun), { buffer = bufnr, noremap = true })
-    end
-    set_keymap("l", expand)
-    set_keymap("h", collapse)
-  end,
+  sync_root_with_cwd = true,
+  diagnostics = {
+    enable = true,
+    debounce_delay = 1000,
+    show_on_dirs = true,
+  },
+  on_attach = attach,
   view = {
-    width = 30,
-    hide_root_folder = false,
+    adaptive_size = false,
+    centralize_selection = true,
     side = "left",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
+    number = true,
+    relativenumber = true,
     signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- user mappings go here
-      },
-    },
   },
   renderer = {
     group_empty = true,
-    indent_markers = {
-      enable = false,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
-      },
-    },
-    icons = {
-      webdev_colors = true,
-    },
-  },
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  ignore_ft_on_setup = {},
-  system_open = {
-    cmd = "",
-    args = {},
-  },
-  diagnostics = {
-    enable = false,
-    show_on_dirs = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-    exclude = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 400,
-  },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    open_file = {
-      quit_on_open = false,
-      resize_window = false,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
-    },
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-    },
   },
 }
 
