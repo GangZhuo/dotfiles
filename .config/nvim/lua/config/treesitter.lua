09569870ca1d4a7cec2524322ab4f36b13df05fc
Gang Zhuo <gang.zhuo@gmail.com>
2022-11-17 23:30:23 +0800

    config nvim-matchup

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -5,4 +5,7 @@ require("nvim-treesitter.configs").setup {
     enable = true, -- false will disable the whole extension
     disable = { 'help' }, -- list of language that will be disabled
   },
+  matchup = {
+    enable = true,
+  },
 }
