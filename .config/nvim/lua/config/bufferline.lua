dd21e8cf087e3e7ffc2af8c170dda4dbabc57f16
Gang Zhuo <gang.zhuo@gmail.com>
2022-11-22 09:07:29 +0800

    mappings for goto buffer

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -1,6 +1,6 @@
 require("bufferline").setup {
   options = {
-    numbers = "buffer_id",
+    numbers = "ordinal",
     close_command = "bdelete! %d",
     right_mouse_command = nil,
     left_mouse_command = "buffer %d",
@@ -44,3 +44,24 @@ require("bufferline").setup {
     sort_by = "id",
   },
 }
+
+-- Go to a certain buffer
+vim.keymap.set("n", "gt", function(args)
+    if vim.v.count == 0 then
+      vim.cmd("bnext")
+    else
+      require("bufferline").go_to_buffer(vim.v.count)
+    end
+end, {
+  desc = "go to buffer by ordinal number",
+})
+vim.keymap.set("n", "<leader>1", "<cmd>lua require('bufferline').go_to_buffer(1)<cr>", { desc = "go to first buffer", })
+vim.keymap.set("n", "<leader>2", "<cmd>lua require('bufferline').go_to_buffer(2)<cr>", { desc = "go to 2nd buffer", })
+vim.keymap.set("n", "<leader>3", "<cmd>lua require('bufferline').go_to_buffer(3)<cr>", { desc = "go to 3rd buffer", })
+vim.keymap.set("n", "<leader>4", "<cmd>lua require('bufferline').go_to_buffer(4)<cr>", { desc = "go to 4th buffer", })
+vim.keymap.set("n", "<leader>5", "<cmd>lua require('bufferline').go_to_buffer(5)<cr>", { desc = "go to 5th buffer", })
+vim.keymap.set("n", "<leader>6", "<cmd>lua require('bufferline').go_to_buffer(6)<cr>", { desc = "go to 6th buffer", })
+vim.keymap.set("n", "<leader>7", "<cmd>lua require('bufferline').go_to_buffer(7)<cr>", { desc = "go to 7th buffer", })
+vim.keymap.set("n", "<leader>7", "<cmd>lua require('bufferline').go_to_buffer(8)<cr>", { desc = "go to 8th buffer", })
+vim.keymap.set("n", "<leader>9", "<cmd>lua require('bufferline').go_to_buffer(9)<cr>", { desc = "go to 9th buffer", })
+vim.keymap.set("n", "<leader>$", "<cmd>lua require('bufferline').go_to_buffer(-1)<cr>", { desc = "go to last buffer", })
