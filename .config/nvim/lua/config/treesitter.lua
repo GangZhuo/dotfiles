8f5c58a74c43d0d0ec85bd46151470ae6be9bb92
Gang Zhuo <gang.zhuo@gmail.com>
2022-11-26 13:06:14 +0800

    change colorscheme

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -3,6 +3,7 @@ require("nvim-treesitter.configs").setup {
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
+    additional_vim_regex_highlighting = false,
     disable = { 'help' }, -- list of language that will be disabled
   },
   matchup = {
