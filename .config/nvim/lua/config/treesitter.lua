1e46e90dfc67896089eced87d3b813373748adef
Gang Zhuo <gang.zhuo@gmail.com>
2022-12-26 00:30:08 +0800

    Install parsers synchronously

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,8 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = "all",
   ignore_install = {}, -- List of parsers to ignore installing
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
   highlight = {
     enable = true, -- false will disable the whole extension
     additional_vim_regex_highlighting = false,
