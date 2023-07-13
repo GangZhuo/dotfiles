3f0eb0827c36da17ca180b44ade5261c66d07ef9
Gang Zhuo <gang.zhuo@gmail.com>
2023-07-13 23:53:24 +0800

    mapping zr,zm to nvim-ufo

--- .config/nvim/lua/config/ufo.lua
+++ .config/nvim/lua/config/ufo.lua
@@ -5,10 +5,11 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+ufo.setup({
+})
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', ufo.openAllFolds)
 vim.keymap.set('n', 'zM', ufo.closeAllFolds)
-
-ufo.setup({
-})
+vim.keymap.set('n', 'zr', ufo.openFoldsExceptKinds)
+vim.keymap.set('n', 'zm', ufo.closeFoldsWith)
