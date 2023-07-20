d99339334ebe7780a928d417654606c124cfb24b
Gang Zhuo <gang.zhuo@gmail.com>
2023-07-20 09:25:34 +0800

    clean plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,42 +1,24 @@
-local api = vim.api
-local fn = vim.fn
 
---- Install packer if it has not been installed.
---- Return:
---- true: if this is a fresh install of packer
---- false: if packer has been installed
-local function packer_ensure_install()
-  -- Where to install packer.nvim -- the package manager (we make it opt)
-  local packer_dir = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
-
-  if fn.glob(packer_dir) ~= "" then
-    return false
+local ensure_packer = function()
+  local fn = vim.fn
+  local api = vim.api
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
   end
-
-  -- Auto-install packer in case it hasn't been installed.
-  vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
-
-  local packer_repo = "https://github.com/wbthomason/packer.nvim"
-  local install_cmd = string.format("!git clone --depth=1 %s %s", packer_repo, packer_dir)
-  vim.cmd(install_cmd)
-
-  return true
+  return false
 end
 
+local packer_bootstrap = ensure_packer()
 
-local fresh_install = packer_ensure_install()
-
--- Load packer.nvim
-vim.cmd("packadd packer.nvim")
-
-local packer = require("packer")
-local packer_util = require("packer.util")
-
-packer.startup {
+require("packer").startup {
   function(use)
 
     -- Packer can manage itself
-    use { "wbthomason/packer.nvim", opt = true }
+    use { "wbthomason/packer.nvim" }
 
     -- A list of colorscheme plugin you may want to try. Find what suits you.
     use { "sainnhe/sonokai",             opt = true }
@@ -167,36 +149,13 @@ packer.startup {
       end,
     }
 
+    if packer_bootstrap then
+      require("packer").sync()
+    end
+
   end,
   config = {
     max_jobs = 4,
-    package_root = packer_util.join_paths(vim.fn.stdpath("data"), "site", "pack"),
-    compile_path = packer_util.join_paths(vim.fn.stdpath("data"), "site", "lua", "packer_compiled.lua"),
   },
 }
-
--- For fresh install, we need to install plugins. Otherwise, we just need to require `packer_compiled.lua`.
-if fresh_install then
-  -- We run packer.sync() here, because only after packer.startup, can we know which plugins to install.
-  -- So plugin installation should be done after the startup process.
-  packer.sync()
-else
-  local status, _ = pcall(require, "packer_compiled")
-  if not status then
-    local msg = "File packer_compiled.lua not found: run PackerSync to fix!"
-    vim.notify(msg, vim.log.levels.ERROR, { title = "nvim-config" })
-  end
-end
-
--- Auto-generate packer_compiled.lua file
-api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = "*/nvim/lua/plugins.lua",
-  group = api.nvim_create_augroup("packer_auto_compile", { clear = true }),
-  callback = function(ctx)
-    local cmd = "source " .. ctx.file
-    vim.cmd(cmd)
-    vim.cmd("PackerCompile")
-    vim.notify("PackerCompile done!", vim.log.levels.INFO, { title = "Nvim-config" })
-  end,
-})
 
