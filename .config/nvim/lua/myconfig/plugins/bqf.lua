799627020d87a0c240a1f522c597d5ff1041ca32
Gang Zhuo <gang.zhuo@gmail.com>
2023-07-22 23:14:37 +0800

    qf windows: allow auto resize height, and enable preview window

--- .config/nvim/lua/myconfig/plugins/bqf.lua
+++ .config/nvim/lua/myconfig/plugins/bqf.lua
@@ -1,6 +1,15 @@
+--[[
+-- * Press <Tab> or <S-Tab> to toggle the sign of item
+-- * Press zn or zN will create new quickfix list
+-- * Press zf in quickfix window will enter fzf mode.
+-- * Press p to toggle auto preview when cursor moved
+-- * Press P to toggle preview for an item of quickfix list
+-- * Press ctrl-t/ctrl-x/ctrl-v to open up an item in a new tab,
+--   a new horizontal split, or in a new vertical split.
+-- * Press ctrl-q to toggle sign for the selected items
+-- * Press ctrl-c to close quickfix window and abort fzf
+
+--]]
 require("bqf").setup {
-  auto_resize_height = false,
-  preview = {
-    auto_preview = false,
-  },
+  auto_resize_height = true,
 }
